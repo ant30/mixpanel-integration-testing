343a66a7c3b4c56ec9fbb97900ea25334460930e
ant30 <ant30tx@gmail.com>
2015-08-10 17:15:33 +0200

    Proof of concept of mixpaneltesting integration with RSpec

---
+++ example/site/spec/mixpanel_spec.rb
@@ -0,0 +1,45 @@
+require_relative "mixpanelconfig_spec"
+require_relative "../app.rb"
+
+describe "Mixpaneltesting environment" do
+  include_context "mixpaneltesting"
+
+  @localurl = ENV["LOCAL_URL"]
+
+  describe "Simple session with one interation (click)" do
+
+    it "Expected 2 page views/1 click" do
+      @selenium.waitfor_object_displayed(:tag_name, 'title')
+      @selenium.click(:link_text => 'Page 1')
+
+      expect(@selenium.get_page_source).to match(/<title>Page 1<\/title>/)
+
+      expect(@mixpanel.validate_results({
+        'PageView' => 2,
+        'ClickInteraction' => 1
+      })).to be true
+    end
+  end
+
+  describe "Full session from init to thankyou page" do
+
+    it "Expected 4 page views/3 clicks" do
+      @selenium.waitfor_object_displayed(:tag_name, 'title')
+
+      @selenium.click(:link_text => 'Page 1')
+      expect(@selenium.get_page_source).to match(/<title>Page 1<\/title>/)
+
+      @selenium.click(:link_text => 'Page 2')
+      expect(@selenium.get_page_source).to match(/<title>Page 2<\/title>/)
+
+      @selenium.click(:link_text => 'Thank you page')
+      expect(@selenium.get_page_source).to match(/<title>Thank you page<\/title>/)
+
+      expect(@mixpanel.validate_results({
+        'PageView' => 4,
+        'ClickInteraction' => 3
+      })).to be true
+    end
+  end
+
+end
---
+++ example/site/spec/mixpanelconfig_spec.rb
@@ -0,0 +1,71 @@
+require_relative "spec_helper"
+require 'rspec/expectations'
+require 'mixpaneltesting'
+
+
+RSpec.configure do |config|
+
+
+  [:docker, :selenium, :mixpanel, :localsite, :localurl].each do |item|
+    config.add_setting item
+  end
+
+  config.before(:suite) do
+    log = Logger.new(STDOUT)
+    log.info "Starting docker, if you don't see started message, try to remove mixpaneltesting docker with docker rm -rf mixpaneltesting"
+
+    docker = MixpanelTesting::DockerProvider.new('firefox', '2.46.0', true)
+    docker.start
+    (1..20).each { |i|
+      sleep 1
+      log.info "Waiting to docker ready: #{i}"
+      break if docker.ready?
+    }
+    log.info "Docker started"
+
+    docker.open_vnc
+
+    localurl = ENV["LOCAL_URL"]
+
+    localsite = MixpanelTesting::LocalSiteProvider.new('PORT=3000 ./start', localurl)
+    localsite.start
+
+    log.info "site started"
+    RSpec.configuration.docker = docker
+    RSpec.configuration.localsite = localsite
+    RSpec.configuration.localurl = localurl
+  end
+
+  config.before(:example) do
+    puts "selenium connection"
+    selenium = MixpanelTesting::SeleniumProvider.new(RSpec.configuration.docker.selenium_uri, :firefox)
+    selenium.connect!
+    selenium.start_session RSpec.configuration.localurl
+    mixpanel = MixpanelTesting::MixpanelProvider.new selenium.session_id
+
+    RSpec.configuration.selenium = selenium
+    RSpec.configuration.mixpanel = mixpanel
+  end
+
+  config.after(:example) do
+    RSpec.configuration.selenium.end_session
+    RSpec.configuration.selenium.quit
+  end
+
+  config.after(:suite) do
+    RSpec.configuration.docker.kill
+    RSpec.configuration.localsite.kill
+  end
+
+end
+
+RSpec.shared_context "mixpaneltesting" do
+  before(:each) do
+    @log = Logger.new(STDOUT)
+    @selenium = RSpec.configuration.selenium
+    @docker = RSpec.configuration.docker
+    @localsite = RSpec.configuration.localsite
+    @localurl = RSpec.configuration.localurl
+    @mixpanel = RSpec.configuration.mixpanel
+  end
+end
